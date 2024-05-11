e7487fa0d5a3dee26652d357ee9ae2596d42a1c6
KIM Kangseong <kim.kangseong@gmail.com>
2024-05-11 12:35:49 +0900

    react-navigation 설치

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './components/screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Home'
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/components/screens/HomeScreen/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function HomeScreen() {
+  return (
+    <View style={{ backgroundColor: 'red', marginTop: 100 }}>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen;
